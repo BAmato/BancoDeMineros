c680051819114f273da90683409621fd3756e505
MrOhms <36096262+BAmato@users.noreply.github.com>
2024-10-24 00:07:59 -0600

    Excel file map population
    Implemented function to create a hashmap and store data from the excel file.
    
    Also made minor changes to main method

--- bankMain.java
+++ bankMain.java
@@ -1,6 +1,13 @@
 import java.util.Scanner;
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+
 
 public class bankMain{
 
@@ -10,8 +17,9 @@ public class bankMain{
     public static void main(String[] args){
 
         boolean exitProgram = false;
-        //int selection = 0;
-    
+
+        createMap();        
+
         customer.setName("Bryan","Amato");
         while(!exitProgram){
             System.out.println("\n-------------El Paso Miners Bank---------------");
@@ -116,4 +124,50 @@ public class bankMain{
             System.out.println("ERROR: "+e);
         }
     }
+
+    static void createMap(){
+
+        String database = "BankUsersActive.csv";            //File name in folder must match this
+        String newFile = "BankUsersNew.csv";                //New file with modified values
+
+        /*Creating a list that will hold all maps containing the database data.
+        The map consists of a string holding the header (column name) and row containing all the headers data.
+        Using a list allows us for dynamic sizing as the size of the excel file is assumed to be unknown*/
+        List<Map<String, String>> myMap = new ArrayList<>();     
+
+        /*Try-catch block to handle any issues encountered when reading the .csv file */
+        try (BufferedReader br = new BufferedReader(new FileReader(database))){
+
+            System.out.println("Reading file: "+database);
+            //The bufferedReader will capture a full row as a string which we can parse and store column values
+            String line;
+            String[] headers = null;
+
+            while((line = br.readLine()) != null){          //While the current row is not null
+
+                String[] values = line.split(",");          //Parse each cell using the comma delimiter
+                
+                /*If the header is null, store all header values. This condition allows us to store the header
+                row only once which will help us store the data according to their type and identify the type
+                of data in each column (assumed to be unknown to the user)*/
+                if(headers == null){                       
+                    headers = values;
+                }
+
+                //Once the header row has been stored, we can populate the hashmap 
+                else{
+                    Map<String, String> row = new HashMap<>();
+                    for(int i = 0; i<headers.length; i++){
+                        row.put(headers[i], values[i]);
+                    }
+                    myMap.add(row);
+                }
+            };
+            System.out.println("File read successfully.");
+        }
+        catch(Exception e){                     //Handle any errors encountered while reading the file
+            System.out.println("Error: "+e);
+        }
+
+    };
 };

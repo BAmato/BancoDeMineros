176286f00b116f71457cf66389554a745b43cf4b
Christin Moreno <98610369+Christi02@users.noreply.github.com>
2024-10-23 21:15:35 -0600

    Added Saving class

--- Saving.java
+++ Saving.java
@@ -1,3 +1,24 @@
-public class Saving {
+public class Saving extends Account{
+    private double totalSavings;
+    private double availableSavings; 
+
+    public Saving(){}
+
+    public void setTotalSavings(double totalSavingsIn){
+        this.totalSavings = totalSavingsIn;
+    }
+    public void setAvailableSavings(double availableSavingsIn){
+        this.availableSavings = availableSavingsIn;
+    }
+    public double getTotalSavings(){
+        return totalSavings;
+    }
+    public double getavailableSavings(){
+        return availableSavings;
+    }
     
+    public void printCurrStatement(){
+        System.out.println("Current Statement: " + totalSavings);
+    }
+
 }

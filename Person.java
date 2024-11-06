c3602555f5cccb106da3b489ebff73fdab6e5451
Christin Moreno <98610369+Christi02@users.noreply.github.com>
2024-11-05 18:13:57 -0700

    Updated Person class

--- Person.java
+++ Person.java
@@ -1,33 +1,72 @@
+
+/**
+ * This is the abstract Person class
+ * A person's personal information will be collected here
+ * @author: Christin Moreno & Team 14
+ * @version: 
+ */
 public abstract class Person {
-    public String name;
-    public int dobYear;
+    private String name;
+    private String dob;
     private String address;
-    private int ssn; 
+    private String phoneNumber; 
 
-    public Person(){}
+    /**
+     * Constructor
+     * Initializes the customer with a name, address, and unique ID.
+     * @param name name for Person
+     * @param dob date of birth for Person
+     * @param address address for Person
+     * @param phoneNumber phone number for Person
+     * 
+     */
 
-    public void setName(String nameIn){
-        this.name = nameIn;
+    public Person(String name, String dob, String address, String phoneNumber){
+        this.name = name;
+        this.dob = dob;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
     }
-    public void setDob(int dobYearIn){
-        this.dobYear = dobYearIn;
+
+    public void setName(String name){
+        this.name = name;
     }
-    public void setAddress(String addressIn){
-        this.address = addressIn;
+    public void setDob(String dob){
+        this.dob = dob;
     }
-    public void setSsn(int ssnIn){
-        this.ssn = ssnIn;
+    public void setAddress(String address){
+        this.address = address;
     }
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+    /**
+     * Returns Person's name
+     * @return String name 
+     */
     public String getName(){
         return name;
     }
-    public int getDobYear(){
-        return dobYear;
+    /**
+     * Returns Person's date of birth
+     * @return String date of birth
+     */
+    public String getDob(){
+        return dob;
     }
+    /**
+     * Returns Person's address
+     * @return String address
+     */
     public String getAddress(){
         return address;
     }
-    public int getSsn(){
-        return ssn;
+    /**
+     * Returns Person's phone number
+     * @return String phone number
+     */
+    public String getPhoneNumber(){
+        return phoneNumber;
     }
 }
